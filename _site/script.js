ff054f1f9d8e8b84c181a351dea9c506e8520a2f
Sage Vousé <svouse@gmail.com>
2023-09-12 13:12:30 -0400

    update font and body text styling

--- _site/script.js
+++ _site/script.js
@@ -1,19 +1,50 @@
-
+//TODO (svouse): clean up onload
 function onload() {
 // Get the modal
 var modal = document.getElementById("myModal");
 
+// Read the JSON
+var data = '[{  "title": "War on the Poor",  "location": "Oakland",  "date": "March 12, 1966",  "tags": "#blackhistory, #oakland, #blackactivism"}, {  "title": "HOLC “redlining” map of the East Bay",  "location": "East Bay",  "tags": "#redlining, #blackhistory, #bayarea"}, {  "title": "Area Description for section of Emeryville, CA",  "location": "Emeryville",  "date": "June 15, 1937",  "tags": "#blackhistory, #oakland, #blackactivism"}, {  "title": "Area Description for section of Oakland, CA.",  "location": "Oakland",  "date": "June 15, 1937",  "tags": "#blackhistory, #oakland, #blackactivism"}, {  "title": "The Power and Peril of Eminent Domain",  "location": "Oakland",  "date": "2023",  "tags": "#blackhistory, #eminentdomain, #displacement, #dispossession, #urbanrenewal"}, {  "title": "Urban cowboy - Black Joy Parade",  "location": "Oakland",  "date": "2022",  "tags": "#blackfutures, #blackjoy"}, {  "title": "The “Hyphy Rail,” by Azlinah Tambu",  "location": "Oakland",  "date": "2023",  "tags": "#gentrification, #blackfutures, #dispossession, #displacement, #oakland, #transportation"}, {  "title": "“Slay” - Black Joy Parade",  "location": "Oakland",  "date": "2022",  "tags": "#blackjoy, #blackfutures"}, {  "title": "Guns but not Milk",  "location": "Oakland",  "date": "2022",  "tags": "#blackhistory, #Oakland"}, {  "title": "Flyer for the “Manifesting a Future of Black Oakland” workshop",  "location": "Oakland",  "date": "2022",  "tags": "#blackfutures, #gentrification, #capitalism"}, {  "title": "Still from interview with Azlinah Tambu of Moms 4 Housing",  "location": "Oakland",  "date": "2022",  "tags": "#housingjustice, #housingisahumanright"}]';
+
+var jsonData = JSON.parse(data);
+
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementsByClassName("myImg");
 var modalImg = document.getElementById("img01");
+var titleText = document.getElementById("title_text");
+var locationText = document.getElementById("location_tag");
+var dateText = document.getElementById("date_tag");
 var captionText = document.getElementById("caption");
+var tagText = document.getElementById("tag");
+
+var test = ["test", "duck", "three", "test", "duck", "three", "test", "duck", "three", "test", "duck", "three"];
 for (i = 0; i < img.length; i++) { 
   img[i].onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.alt;
+    var title = test[i];
+  console.log(title);
+    titleText.innerHTML = test[i];
   }
 }
+/*
+for (i = 0; i < img.length; i++) { 
+ tags = jsonData[i].tags;
+  title = jsonData[i].title;
+    location = jsonData[i].location;
+    date = jsonData[i].date;
+  img[i].onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    titleText.innerHTML = title;
+    locationText.innerHTML = location;
+    dateText.innerHTML = date;
+    captionText.innerHTML = this.alt;
+    tagText.innerHTML = tags;
+  }
+}
+*/
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -22,5 +53,5 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() { 
   modal.style.display = "none";
 }
-
+console.log(JSON.stringify(data));
 }
